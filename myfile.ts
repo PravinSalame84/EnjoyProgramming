fb5ff524868a94a35fd615b8c7518bd29ec1502b
Pravin Salame <pravin.salame@utilizecore.com>
2025-03-22 17:26:07 +0530

    adding file for initial commit

---
+++ myfile.ts
@@ -0,0 +1,37 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2761
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Arial-BoldMT;\f1\fswiss\fcharset0 ArialMT;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;\red255\green255\blue255;\red235\green237\blue255;
+\red163\green184\blue255;}
+{\*\expandedcolortbl;;\cssrgb\c0\c1\c1;\cssrgb\c100000\c100000\c99985\c0;\cssrgb\c93607\c94401\c100000;
+\cssrgb\c69843\c77735\c100000;}
+{\*\listtable{\list\listtemplateid1\listhybrid{\listlevel\levelnfc23\levelnfcn23\leveljc0\leveljcn0\levelfollow0\levelstartat1\levelspace360\levelindent0{\*\levelmarker \{disc\}}{\leveltext\leveltemplateid1\'01\uc0\u8226 ;}{\levelnumbers;}\fi-360\li720\lin720 }{\listlevel\levelnfc23\levelnfcn23\leveljc0\leveljcn0\levelfollow0\levelstartat1\levelspace360\levelindent0{\*\levelmarker \{circle\}}{\leveltext\leveltemplateid2\'01\uc0\u9702 ;}{\levelnumbers;}\fi-360\li1440\lin1440 }{\listname ;}\listid1}}
+{\*\listoverridetable{\listoverride\listid1\listoverridecount0\ls1}}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\tx220\tx720\pardeftab720\li720\fi-720\partightenfactor0
+\ls1\ilvl0
+\f0\b\fs24 \cf2 \cb3 {\listtext	\uc0\u8226 	}\cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 Key Concepts:
+\f1\b0 \AppleTypeServices\AppleTypeServicesF65539 \cf2 \cb3 \strokec4 \
+\pard\tx940\tx1440\pardeftab720\li1440\fi-1440\sa160\partightenfactor0
+\ls1\ilvl1
+\f0\b \AppleTypeServices\AppleTypeServicesF65539 \cf2 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\uc0\u9702 	}\cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 Version Control:
+\f1\b0 \AppleTypeServices\AppleTypeServicesF65539 \cf2 \cb3 \strokec4 \'a0Tracking changes to files over time.\'a0\cf2 \cb3 \strokec5 \uc0\u8232 \cf2 \cb3 \strokec4 \
+\ls1\ilvl1
+\f0\b \AppleTypeServices\AppleTypeServicesF65539 \cf2 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\uc0\u9702 	}\cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 Change Control:
+\f1\b0 \AppleTypeServices\AppleTypeServicesF65539 \cf2 \cb3 \strokec4 \'a0Managing and approving changes to the software.\'a0\cf2 \cb3 \strokec5 \uc0\u8232 \cf2 \cb3 \strokec4 \
+\ls1\ilvl1
+\f0\b \AppleTypeServices\AppleTypeServicesF65539 \cf2 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\uc0\u9702 	}\cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 Configuration Identification:
+\f1\b0 \AppleTypeServices\AppleTypeServicesF65539 \cf2 \cb3 \strokec4 \'a0Identifying and documenting the specific versions of software components.\'a0\cf2 \cb3 \strokec5 \uc0\u8232 \cf2 \cb3 \strokec4 \
+\pard\tx940\tx1440\pardeftab720\li1440\fi-1440\partightenfactor0
+\ls1\ilvl1
+\f0\b \AppleTypeServices\AppleTypeServicesF65539 \cf2 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\uc0\u9702 	}\cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 Configuration Audits:
+\f1\b0 \AppleTypeServices\AppleTypeServicesF65539 \cf2 \cb3 \strokec4 \'a0Verifying that the software configuration is correct and meets requirements.\'a0\
+\pard\tx220\tx720\pardeftab720\li720\fi-720\partightenfactor0
+\ls1\ilvl0\cf2 \cb3 \kerning1\expnd0\expndtw0 \outl0\strokewidth0 {\listtext	\uc0\u8226 	}\cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec4 \uc0\u8232 \AppleTypeServices \cf2 \cb3 \strokec4 \uc0\u8232 \u8232 \
+}
